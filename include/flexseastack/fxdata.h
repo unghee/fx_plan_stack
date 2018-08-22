3fa3b06de3d6da0e3290bf0e2d1a315cd7bab1dc
Sébastien Bélanger <sebastien.belan@robotiq.com>
2018-08-22 09:07:36 -0400

    Fixed an issue that was causing the buffer to not use the whole range of memory created. it was causing the buffer declared N size to only be N-1 size in reality.

--- include/flexseastack/fxdata.h
+++ include/flexseastack/fxdata.h
@@ -27,7 +27,7 @@ struct FxDevData {
 
 		// increase size conditionally
 		// advance read index conditionally
-		if((++size) >= _rows)
+        if((++size) > _rows)
 		{
 			--size;
 			if((++rIdx) >= _rows) 
