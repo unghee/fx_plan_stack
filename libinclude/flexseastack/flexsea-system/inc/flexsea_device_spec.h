136dbe178ae184d648e934999b699236b07f4e69
Dan <dtscbs@verizon.net>
2019-01-22 11:30:31 -0500

    [STACK-48] Merged the files from include/flexseastack/flexsea-system/inc to libinclude/flexseastack/flexsea-system/inc/

--- libinclude/flexseastack/flexsea-system/inc/flexsea_device_spec.h
+++ libinclude/flexseastack/flexsea-system/inc/flexsea_device_spec.h
@@ -29,26 +29,39 @@ typedef struct FlexseaDeviceSpec_s {
 /* Specs exist for the following devices */
 #define NUM_DEVICE_TYPES 4
 
-
+typedef enum {
+    FX_NONE = 0,
+    FX_RIGID = 1,
+    FX_EXECUTE = 2,
+    FX_MANAGE = 3,
+    FX_CUSTOM = 99
+} FlexseaDeviceType;
 
 /* this array contains all the device specs */
 extern FlexseaDeviceSpec deviceSpecs[NUM_DEVICE_TYPES];
 
 /* defines and externs needed by plan only */
 #ifdef BOARD_TYPE_FLEXSEA_PLAN
+	#define MAX_CONNECTED_DEVICES 10
+	extern FlexseaDeviceSpec connectedDeviceSpecs[MAX_CONNECTED_DEVICES];
+	extern uint8_t* deviceData[MAX_CONNECTED_DEVICES];
+	extern uint8_t fx_spec_numConnectedDevices;
 	void initializeDeviceSpecs();
+	#ifdef BIG_ENDIAN
 #else
-
-	extern const uint16_t const* read_num_fields_active;
-	extern const uint8_t const* const* read_device_active_field_pointers;
-	extern const uint8_t const* read_device_active_field_lengths;
-	void setActiveFieldsByMap(uint32_t *map);
+		#define DEVICESPEC_TYPE_BY_IDX(i) ( *(deviceData[i]) )
+		#define DEVICESPEC_UUID_BY_IDX(i) ( *((uint16_t*)(deviceData[i] + 1)) )
+	#endif
+#else
 
 #endif //BOARD_TYPE_FLEXSEA_PLAN
 
+void addConnectedDevice(uint8_t devType, uint16_t devId);
+void initializeDeviceSpecs();
 extern const FlexseaDeviceSpec *fx_this_device_spec;
 extern const uint8_t** _dev_data_pointers;
-extern uint32_t *fx_dev_timestamp;
+extern uint16_t fx_dev_id;
+extern uint8_t fx_dev_type;
 
 /* Related to max number of fields, should probably call it max num fields.. */
 #define FX_BITMAP_WIDTH_C 3
