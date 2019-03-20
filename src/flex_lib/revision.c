716f184d0e9059e3ef07bb6385117c5b37eb0e7c
Mike Murray <mmurray393@gmail.com>
2019-03-20 14:23:14 -0400

    removing flex shared lib repo to reduce complexity of nested submodules

---
+++ src/flex_lib/revision.c
@@ -0,0 +1,5 @@
+#include "revision.h"
+#include "git_rev_data.h"
+
+char FLEXSEA_LIB_DATE[] = { "BUILD_DATE: " __DATE__ " " __TIME__};
+char FLEXSEA_LIB_GIT_INFO[] = { "GIT_REVISION: " GIT_REV_DATA };
