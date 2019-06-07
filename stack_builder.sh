8cf16d9a2d1d95562bb1a0b1d302d7ee3f40d2ce
Mike Murray <mmurray393@gmail.com>
2019-06-07 14:56:22 -0400

    this is not good for me on linux

--- stack_builder.sh
+++ stack_builder.sh
@@ -3,7 +3,7 @@
 rm -rf build
 mkdir -p build
 cd build
-cmake .. -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=MINGW7-3_X86_TOOLCHAIN_FILE
+cmake .. -G "Ninja" #-DCMAKE_TOOLCHAIN_FILE=MINGW7-3_X86_TOOLCHAIN_FILE
 # scan-build -v --use-cc=gcc --use-c++=g++ ninja
 ninja
 cd ..
