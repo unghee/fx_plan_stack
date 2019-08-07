ddb227acec32cfc7b748fd1bc62aaab3fba1d93d
Mike Murray <mmurray393@gmail.com>
2019-08-07 10:33:29 -0400

    stack builder now automatically copies libraries to libs directory

--- stack_builder.sh
+++ stack_builder.sh
@@ -1,8 +1,30 @@
-#!/bin/sh
+#!/bin/bash
+
+# Exit this script immediately upon any error.
+set -e
+
+echo "Plan stack builder at your service :)"
 
 rm -rf build
 mkdir -p build
 cd build
-cmake .. -G "Ninja" # -DCMAKE_TOOLCHAIN_FILE=CMAKE_RASPBERRY_PI_TOOLCHAIN_FILE
+cmake .. -G "Ninja"
 ninja
 cd ..
+
+# guest host OS
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	HOST_OS="linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	HOST_OS="mac"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+	HOST_OS="windows"
+elif [[ "$OSTYPE" == "msys" ]]; then
+	HOST_OS="windows"
+else
+	HOST_OS="linux"
+fi
+
+cp build/libs/* libs/${HOST_OS}
+
+exit 0
